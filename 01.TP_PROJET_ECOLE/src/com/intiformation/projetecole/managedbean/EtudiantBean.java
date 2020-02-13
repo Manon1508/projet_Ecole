1beb7fa6b44c836cf08bd64d1dc26cab6d4c34af
IN-BR-026 <IN-BR-026@IN-BR-026>
2020-02-13 18:00:53 +0100

    test commit via spring 13/02

--- 01.TP_PROJET_ECOLE/src/com/intiformation/projetecole/managedbean/EtudiantBean.java
+++ 01.TP_PROJET_ECOLE/src/com/intiformation/projetecole/managedbean/EtudiantBean.java
@@ -43,6 +43,8 @@ public class EtudiantBean implements Serializable{
 	
 	// dao de etudiant
 	private EtudiantDao etudiantDao;
+	
+	
 	/*ctor */
 
 	/**
---
+++ 01.TP_PROJET_ECOLE/src/com/intiformation/projetecole/testcommit/toto.java
@@ -0,0 +1,5 @@
+package com.intiformation.projetecole.testcommit;
+
+public class toto {
+
+}
