3fb2e5dbc7220eacd2adba9d5ffcbbecb4755b1c
IN-BR-026 <IN-BR-026@IN-BR-026>
2020-02-18 18:14:04 +0100

    manon 18_02

---
+++ 01.TP_PROJET_ECOLE/src/com/intiformation/projetecole/managedbean/AuthentificationAdminBean.java
@@ -0,0 +1,88 @@
+package com.intiformation.projetecole.managedbean;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+
+
+import com.intiformation.projetecole.dao.AdministrateurDao;
+import com.intiformation.projetecole.tool.SessionUtils;
+
+@ManagedBean(name="authentificationBean")
+@SessionScoped
+public class AuthentificationAdminBean {
+	
+	private String identifiant;
+	private String motDePasse;
+	
+	private AdministrateurDao administrateurDao;
+	
+	public AuthentificationAdminBean() {
+		administrateurDao = new AdministrateurDao();
+	}
+	
+	
+	public String connecterAdmin() {
+		if(administrateurDao.isExist(identifiant, motDePasse)) {
+			// --> user exist
+			// creation de la session
+			
+			HttpSession session = SessionUtils.getUserSession();
+			//sauvegarde du login dans la session
+			session.setAttribute("user_login", identifiant);
+			
+			// renvoie de la page de redirection
+			return "menuAdmin.xhtml";
+		}else {
+		    // --> user not exist
+			// => message d'erreur vers la vue
+			FacesContext context = FacesContext.getCurrentInstance();
+			context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, "Echec de la connexion", "Identifiant ou mot de passe invalide"));
+			
+			// redirection
+			return "codeAdmin.xhtml";
+		}// end else
+	}// end method
+	
+	public String deconnecterAdmin() {
+		//recup de la session
+		HttpSession session = SessionUtils.getUserSession();
+		
+		// deconnexion
+		session.invalidate();
+		
+		// redirection
+		return "codeAdmin.xhtml";
+	}// end metrhod
+
+	public String getIdentifiant() {
+		return identifiant;
+	}
+
+	public void setIdentifiant(String identifiant) {
+		this.identifiant = identifiant;
+	}
+
+	public String getMotDePasse() {
+		return motDePasse;
+	}
+
+	public void setMotDePasse(String motDePasse) {
+		this.motDePasse = motDePasse;
+	}
+
+	public AdministrateurDao getAdministrateurDao() {
+		return administrateurDao;
+	}
+
+	public void setAdministrateurDao(AdministrateurDao administrateurDao) {
+		this.administrateurDao = administrateurDao;
+	}
+	
+	
+	
+
+}
